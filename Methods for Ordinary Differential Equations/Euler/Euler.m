e46220f09796c4432fc52b1657f23e624b4cbc01
xiaowangggg <140183564+xiaowangggg@users.noreply.github.com>
2024-03-20 13:30:41 +0800

    Euler.m

--- Methods for Ordinary Differential Equations/Euler/Euler.m
+++ Methods for Ordinary Differential Equations/Euler/Euler.m
@@ -1,16 +1,16 @@
 %%欧拉法的主程序
-function [x,u]=Euler(h)
+function [x,y]=Euler(h)
    x1=1;
    x2=2;
-   u1=0;
+   y1=0;
    N=abs(x2-x1)/h;
-   u=zeros(N+1,1);
+   y=zeros(N+1,1);
    x=zeros(N+1,1);
-   u(1)=u1;
+   y(1)=y1;
    x(1)=x1;
    for i=1:N
        x(i+1)=x(i)+h;
-       u(i+1)=u(i)+h*f(x(i),u(i));
+       y(i+1)=y(i)+h*f(x(i),y(i));
    end
 end
 
