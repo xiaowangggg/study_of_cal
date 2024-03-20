1117055f20389fde129aab58afd2beb3b20fb70e
xiaowangggg <140183564+xiaowangggg@users.noreply.github.com>
2024-03-20 13:34:37 +0800

    Euler_backward.m

--- Methods for Ordinary Differential Equations/Euler/Euler_backward.m
+++ Methods for Ordinary Differential Equations/Euler/Euler_backward.m
@@ -1,17 +1,17 @@
 %%向后欧拉法的主程序
-function [x,u]=Euler_backward(h)
+function [x,y]=Euler_backward(h)
      x1=1;
      x2=2;
-     u1=0;
+     y1=0;
      N=abs(x2-x1)/h;
-     u=zeros(N+1,1);
+     y=zeros(N+1,1);
      x=zeros(N+1,1);
-     u(1)=u1;
+     y(1)=y1;
      x(1)=x1;
      for i=1:N
          x(i+1)=x(i)+h;
-         tmp_u=u(i)+h*f(x(i),u(i));
-         u(i+1)=Euler_iteration(x(i),u(i),tmp_u,h,0.001);
+         tmp_u=y(i)+h*f(x(i),u(i));
+         y(i+1)=Euler_iteration(x(i),y(i),tmp_u,h,0.001);
      end
 end
 
